7c6a172a8d7a94fd5dae1f9fe33306fe8158d458
Noel Feliciano <feliciano.noel@gmail.com>
2016-04-06 03:23:12 -0700

    numbers are displaying on calendar properly depending on date

--- NFCalendar/NFCalendar.swift
+++ NFCalendar/NFCalendar.swift
@@ -46,17 +46,36 @@ class NFCalendar: UIView, UICollectionViewDelegate, UICollectionViewDataSource, 
     // MARK: UICollectionViewDataSource
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         let cell:NFCalendarCell = collectionView.dequeueReusableCellWithReuseIdentifier("Cell", forIndexPath: indexPath) as! NFCalendarCell
+        let date = NSDate()
+        let firstDay = date.firstDayOfMonth().whichWeekday()
+        if (indexPath.row < firstDay-1) {
+            let lastMonth: NSDate!
+            if (date.whichMonth() == 1) {
+                lastMonth = NSDate(year: date.whichYear()-1, month: 12, day: 1)
+            } else {
+                lastMonth = NSDate(year: date.whichYear(), month: date.whichMonth()-1, day: 1)
+            }
+            
+            // -1 to account for 'whichWeekday' being a nonZero number, and -1 to account for indexPath.row starting at 0
+            cell.cellLabel.text = "\(lastMonth.numDaysInMonth()-(firstDay-2)+indexPath.row)"
+            cell.cellLabel.textColor = UIColor.grayColor()
+            
+            //TODO: get number of days from last month
+        } else {
+            cell.cellLabel.text = "\(indexPath.row-firstDay+2)"
+        }
+        
+        //TODO: add in leftover days for next month
         return cell
     }
 
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        let today = NSDate()
-        return today.numCalendarSlotsInMonth()
+        return NSDate().numCalendarSlotsInMonth()
     }
     
     func collectionView(collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, atIndexPath indexPath: NSIndexPath) -> UICollectionReusableView {
         let header:NFCalendarHeader = collectionView.dequeueReusableSupplementaryViewOfKind(UICollectionElementKindSectionHeader, withReuseIdentifier: "Header", forIndexPath: indexPath) as! NFCalendarHeader
-        header.monthLabel.text = "January"
+        header.monthLabel.text = "April"
         return header
     }
     
--- NFCalendar/NSDateExtensions.swift
+++ NFCalendar/NSDateExtensions.swift
@@ -9,7 +9,17 @@
 import Foundation
 
 extension NSDate {
-    // Get the number of days for the current month
+    
+    convenience init( year:Int, month:Int, day:Int) {
+        let calendar = NSCalendar.currentCalendar()
+        let dateComponent = NSDateComponents()
+        dateComponent.year = year
+        dateComponent.month = month
+        dateComponent.day = day
+        self.init(timeInterval:0, sinceDate:calendar.dateFromComponents(dateComponent)!)
+    }
+    
+    // Get the number of days for the month
     func numDaysInMonth() -> Int {
         let calendar = NSCalendar.currentCalendar()
         let days = calendar.rangeOfUnit(NSCalendarUnit.Day, inUnit: NSCalendarUnit.Month, forDate: self)
@@ -24,7 +34,26 @@ extension NSDate {
     }
     
     func numCalendarSlotsInMonth() -> Int {
+        print("\(firstDayOfMonth().whichMonth())")
         return numDaysInMonth() + firstDayOfMonth().whichWeekday() - 1
+    }
+    
+    func whichYear() -> Int {
+        let calendar = NSCalendar.currentCalendar()
+        let dateComponent = calendar.components(.Year, fromDate: self)
+        return dateComponent.year
+    }
+    
+    func whichMonth() -> Int {
+        let calendar = NSCalendar.currentCalendar()
+        let dateComponent = calendar.components(.Month, fromDate: self)
+        return dateComponent.month
+    }
+    
+    func whichDay() -> Int {
+        let calendar = NSCalendar.currentCalendar()
+        let dateComponent = calendar.components(.Day, fromDate: self)
+        return dateComponent.day
     }
     
     // Returns an int of which day of the week it is, 1 for Sunday
